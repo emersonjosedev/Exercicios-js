2ea62a016bec70c6a30ef2f0e63dd74ec7138ad6
emersonjosedev <emersonmenjob@gmail.com>
2025-04-04 13:20:16 -0300

    0.01

---
+++ ex/ex002.js
@@ -0,0 +1,5 @@
+// 2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+const prompt = require('prompt-sync')();
+const nome = prompt('Digite seu nome: ');
+console.log(`Olá, ${nome}`);
